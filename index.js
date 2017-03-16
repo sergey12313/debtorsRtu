561b75172c3220d3d4b1f62e00423798e816ac46
Sergey Razuvaev <sergey12313@gmail.com>
2017-03-16 16:36:58 +0300

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 'use strict';
-
 const db = require('sqlite');
+const Log=require('log');
+const fs = require('fs');
+const {logPath,sqliteFile}=require('./configs/configs.json');
 const {difference,range} = require('lodash');
 const XmlStringGen=require("./lib/xmlStringGen");
 const rtuRequest= require('./lib/RtuHttpRequest');
 const ParseRtuXml=require('./lib/ParseRtuXml');
+const logFile=`${logPath}${Date.now()}.log`;
+const log= new Log('debug',fs.createWriteStream(logFile));
 
-async function on() {
+async function on(arr) {
+    if(arr.length===0){
+        return true;
+    }
+    for(let i=0;i<arr.length;i++){
+        let item=arr[i];
+        let settingsFromDb=await db.get(`SELECT * from deb WHERE id=${item}`);
+        settingsFromDb.groups=JSON.parse(settingsFromDb.groups);
+
+        let XmlSetDef=await XmlStringGen(item).setCapacityAndGroups(settingsFromDb);
+        console.log(XmlSetDef);
+    }
 
 }
 
@@ -27,39 +42,49 @@ async function off(arr) {
             if(e.extra===404){
                 parsedGroupAndCapacity={"id":item,"groups":{},"capacity":0,"found":0}
             }
-            else {
-                throw (e);
-            }
+            throw (e );
         }
         let {id,capacity}=parsedGroupAndCapacity;
         let groups= JSON.stringify(parsedGroupAndCapacity.groups);
         let found= (parsedGroupAndCapacity.hasOwnProperty('found'))?parsedGroupAndCapacity.found:1;
-        let run = await db.run(`INSERT INTO deb (id,groups,capacity,found) VALUES (${id}, '${groups}',${capacity} ,${found})`);
-        //console.log(run);
+        if (found===1){
+            let xmlSetDeb=await XmlStringGen(item).setDebNum();
+            let resSetDeb=await rtuRequest(xmlSetDeb);
+            let parsedSetDebResult;
+            try {
+                parsedSetDebResult=await ParseRtuXml.UserEditResult(resSetDeb);
+            }
+            catch (e){
+                log.error(e);
+                continue;
+            }
+        }
+        await db.run(`INSERT INTO deb (id,groups,capacity,found) VALUES (${id}, '${groups}',${capacity} ,${found})`);
     }
-
+    return true
 }
 async function run() {
-    let arr1=range(20000,30000);
+   /* let arr1=range(20000,30000);
     let arr2=range(70000,80000);
     let arr3=range(90000,95000);
     let arr=[...arr1,...arr2,...arr3];
-    console.log(arr.length);
+    console.log(arr.length);*/
 
 
-    let sybaseNums = await Promise.resolve(arr);
-    await db.open('./configs/db.sqlite');
+    let sybaseNums = await Promise.resolve([...range(16670,16675),/*...[20000]*/]);////need big exception
+    await db.open(sqliteFile);
     // await db.run(`DROP TABLE deb`);
-    //await db.run(`CREATE TABLE deb(id INT PRIMARY KEY,groups TEXT,capacity INT,found INT DEFAULT 1)`);
+   // await db.run(`CREATE TABLE deb(id INT PRIMARY KEY,groups TEXT,capacity INT,found INT DEFAULT 1)`);
     let result = await db.all('SELECT * FROM deb');
     let localNums =result.map((item)=>{
         return item.id
     });
     let needOff = difference(sybaseNums,localNums);
     let needOn = difference(localNums,sybaseNums);
-    let offResult;
+    let offResult,onResult ;
     try{
        offResult =await off(needOff);
+       onResult =await on(needOn);
     }
     catch (e){
         console.log(e);
---
+++ setWebpassword.js
@@ -0,0 +1,23 @@
+'use strict';
+const XmlStringGen=require("./lib/xmlStringGen");
+const rtuRequest= require('./lib/RtuHttpRequest');
+const ParseRtuXml=require('./lib/ParseRtuXml');
+ const arr=[
+
+
+ ];
+
+
+
+ async function setPass() {
+     arr.forEach(async(item)=>{
+         let xml =await XmlStringGen(item).setWebPassword();
+         console.log(xml);
+         let res =await rtuRequest(xml);
+         let result = await ParseRtuXml.UserEditResult(res);
+         console.log(result);
+     });
+
+
+ }
+ setPass();
